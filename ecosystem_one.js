4c853ea37246b3f3734ce226bf4b2b232a8748d0
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2023-05-25 17:35:50 -0400

    added carnivores and herbivores

--- ecosystem_one.js
+++ ecosystem_one.js
@@ -1,27 +1,51 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 const math = require('canvas-sketch-util/math');
+
+const randomWords = require('random-words')
 const Tweakpane = require('tweakpane')
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 2048, 2048 ],
+  animate: true,
+  fps: 30
 };
 
+let seconds = 0
+let previousSeconds = 0;
+
 const params = {
-  population: 200,
-  radius: 4,
+  population: 300,
+  radius: 8,
 }
+
+const updateTime = (frame) => {
+  if (frame % 30 === 0) {
+    seconds += 1
+  }
+  return true;
+}
+
+
 
 const sketch = ({context, width, height}) => {
 
   const organismsArr = [];
   const organisms = populate(organismsArr, params.population, width, height)
-  return ({ context, width, height }) => {
+  return ({ context, width, height, frame }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
-    
+
+    updateTime(frame);
+
     organisms.forEach(bebe => {
       bebe.update();
-      bebe.draw(context);
+      if (bebe instanceof Carnivore) {
+        bebe.draw(context, 'carnivore');
+      } else if (bebe instanceof Herbivore) {
+        bebe.draw(context, 'herbivore')
+      } else {
+        bebe.draw(context);
+      }
       bebe.bounce(width, height);
     })
   };
@@ -29,11 +53,33 @@ const sketch = ({context, width, height}) => {
 };
 
 function populate (organisms, population, width, height) {
-  for (let i = 0; i < population; i++) {
-    const x = random.range(0, width);
-    const y = random.range(0, height);
+  let fauna = (population / 3);
+  let flora = (population / 3)  * 2;
+  for (let i = 0; i < (Math.floor(fauna * 0.7)); i++) {
+    let name = randomName()
+    let x = randomWidth(width)
+    let y = randomHeight(height);
+    let vel = randomVector(width, height);
+    let sex = randomSex();
 
-    organisms.push(new Organism(x, y));
+    organisms.push(new Herbivore(name, x, y, vel, sex));
+  }
+
+  for (let i = 0; i < (Math.floor(fauna * 0.3)); i++) {
+        let name = randomName()
+
+    let x = randomWidth(width)
+    let y = randomHeight(height);
+    let vel = randomVector(width, height);
+    let sex = randomSex();
+
+    organisms.push(new Carnivore(name, x, y, vel, sex));
+  }
+  
+  for (let i = 0; i < flora; i++) {
+    let pos = randomVector(width, height);
+
+    organisms.push(new Flora(pos));
   }
 
   return organisms
@@ -47,6 +93,20 @@ const createPane = () => {
 }
 
 createPane();
+
+function randomWidth (width) {
+  return random.range(0, width);
+}
+
+function randomHeight (height) {
+  return random.range(0, height);
+}
+
+function randomVector (width, height) {
+  const x = randomWidth(width)
+  const y = randomHeight(height);
+  return new Vector(x, y)
+}
 
 class Vector {
   constructor(x, y) {
@@ -71,38 +131,74 @@ class Organism {
     this.pos.y += this.vel.y;
   }
 
-  draw(context) {
+  draw(context, type = 'general') {
     context.save();
     context.translate(this.pos.x, this.pos.y);
     context.lineWidth = 1;
-    
     context.beginPath();
-    context.arc(0, 0, params.radius, 0, Math.PI * 2);
-    context.fill();
+    if (type === 'general') {
+      context.arc(0, 0, params.radius, 0, Math.PI * 2);
+      context.fill();
+    } else if (type === 'carnivore') {
+      context.rect(0, 0, params.radius, params.radius);
+      context.fillStyle = 'red'
+      context.fill();
+    } else if (type === 'herbivore') {
+      context.arc(0, 0, params.radius, 0, Math.PI * 2);
+      context.fillStyle = 'green'
+      context.fill();
+    }
+    
     context.stroke();
-
     context.restore();
   }
 }
 
+function randomSex () {
+  let coin = Math.floor(Math.random * (2))
+  if (coin < 0.5) {
+      return 'M'
+    } else {
+      return 'F'
+    };
+  }
+
 class Fauna extends Organism {
-  constructor(vel) {
-    super(pos);
-    this.vel = vel;
+  constructor(x, y) {
+    super(x, y)
+    this.sex = randomSex();
+    this.hunger = 100;
+  }
+
+  getHungry () {
+    if (updateTime()) {
+      this.hunger -= 0.02
+    }
   }
 }
 
+const randomName = () => {
+  return randomWords({exactly: 1})
+}
+
 class Herbivore extends Fauna {
-  constructor(name) {
-    super(pos);
-    super(vel);
+  constructor(name, x, y, sex) {
+    super(x, y, sex)
+    this.name = name;
+  }
+}
+
+class Carnivore extends Fauna {
+  constructor(name, x, y, sex) {
+    super(x, y, sex)
     this.name = name;
   }
 }
 
 class Flora extends Organism {
-  constructor(name) {
-    super(pos);
+  constructor(name, pos) {
+    super(pos, pos);
+    this.vel = new Vector(0, 0)
     this.name = name;
   }
 }
@@ -110,7 +206,10 @@ class Flora extends Organism {
 class Funga extends Organism {
   constructor(name) {
     super(pos);
+    this.vel = new Vector(0, 0);
     this.name = name;
   }
 }
+
+
 canvasSketch(sketch, settings);
